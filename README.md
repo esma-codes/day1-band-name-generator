f634992a6361119c291d2135dc2a94cdd238fa94
Esma <esmanurgorgec34@gmail.com>
2025-09-07 23:07:19 +0300

    Day 1 - Band Name Generator
    A simple Python project that generates a fun band name based on user input.

---
+++ README.md
@@ -0,0 +1,13 @@
+# 🎶 Band Name Generator
+
+A simple Python project that generates a fun band name based on user input.  
+
+---
+
+## 🚀 How It Works
+1. 📝 Asks the user for their name  
+2. 🌆 Asks the user for the city where they grew up  
+3. 🐶 Asks the user for the name of their pet  
+4. 🎤 Combines the answers to create a potential **band name**  
+
+---
---
+++ main.py
@@ -0,0 +1,11 @@
+# Band Name Generator
+print("welcome to the band name generator!")
+name = input("what is your name ?")
+print("Hello!" + name + "!" )
+city = input("where did you grow up ?")
+pet = input("what is your pet 's name ?")
+band_name = city + " " + pet
+print("\nyour band name could be : " + band_name )
+print("nice to meet you " + name + "!")
+
+
